b66fa69c2b3c4fb6ec6d8e7dc101de8ccd8f877d
davemeyer <david.w.meyer21@gmail.com>
2021-11-22 18:08:32 -0800

    with web project

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export const App = () => {
+  return (
+    <div>
+      can you see me?
+      <div>
+        <button onClick={() => console.log('HEI')}>click</button>
+      </div>
+    </div>
+  );
+}
